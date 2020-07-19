d8a9575f5c742a110ca412519f71e2bce5a41ee3
Abijah Kabiro <abijahkabiro@gmail.com>
2020-07-19 12:17:35 +0300

    add some function

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function name(){
+    var CC=parseInt(document.getElementById("century").value); 
+    var YY=parseInt(document.getElementById("year").value); 
+    var MM=parseInt(document.getElementById("month").value); 
+    var DD=parseInt(document.getElementById("day").value);
+
+    var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+
+
+
+}
