a1f74a54459d419fb5dbe420fbc68358d38c6b80
Abijah Kabiro <abijahkabiro@gmail.com>
2020-07-19 19:42:15 +0300

    add a function day

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
-var  century ;
-var year;
-var month;
-var date;
+var  CC;
+var YY;
+var MM;
+var DD;
 var dayOfTheWeek ;
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Afua","Afua","Ama"];
 
+var day = function(){
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    day= parseInt(document.getElementById("day").value);
+}
 
