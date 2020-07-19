967a11d0870bea178ba78d00ddf80e1ddf9d2854
Abijah Kabiro <abijahkabiro@gmail.com>
2020-07-19 19:38:56 +0300

    add variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var  century ;
 var year;
 var month;
-var day;
+var date;
 var dayOfTheWeek ;
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Afua","Afua","Ama"];
+
+
